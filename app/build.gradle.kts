5a81fee87a3b454f8c25ae31f48290a075f14453
Bankey <104624750+2646044472@users.noreply.github.com>
2026-08-31 21:36:06 +0800

    feat: add services and work vocabulary

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "dev.local.yuecal"
         minSdk = 31
         targetSdk = 35
-        versionCode = 35
-        versionName = "0.19.0"
+        versionCode = 36
+        versionName = "0.20.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
