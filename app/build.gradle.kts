5551aaa556b75fa9ad75f13554d23c185313358d
bankey <bankey2646044472@gmail.com>
2026-08-31 18:19:16 +0800

    feat: add daily status expressions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "dev.local.yuecal"
         minSdk = 31
         targetSdk = 35
-        versionCode = 33
-        versionName = "0.17.0"
+        versionCode = 34
+        versionName = "0.18.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
