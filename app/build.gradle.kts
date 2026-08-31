e12afc65283404df04039eb5fde8efe7ad55fac9
bankey <bankey2646044472@gmail.com>
2026-08-31 16:36:25 +0800

    release: 0.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "dev.local.yuecal"
         minSdk = 31
         targetSdk = 35
-        versionCode = 23
-        versionName = "0.7.7"
+        versionCode = 24
+        versionName = "0.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
